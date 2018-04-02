f65365e7c99d62e6647365d91cf0d069767445fc
Piyush Bhalala <bhalalapj@gmail.com>
2018-04-02 23:39:00 +0530

    Added basic comp
    Added basic components

--- TestApp/TestApp/ViewController.swift
+++ TestApp/TestApp/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblTitle: UILabel!
+    @IBOutlet weak var btnOk: UIButton!
+    @IBOutlet weak var btnCancel: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -17,6 +20,12 @@ class ViewController: UIViewController {
         
     }
 
+    @IBAction func btnOkClick(_ sender: UIButton) {
+        lblTitle.text = "Hello World !"
+    }
+    @IBAction func btnCancelClick(_ sender: UIButton) {
+        lblTitle.text = "";
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
